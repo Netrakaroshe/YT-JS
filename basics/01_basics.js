706add764dcfe7c59267a370a7ad7f15b9cac12f
Netrakaroshe <netrakaroshe24@gmail.com>
2025-04-13 06:16:55 +0000

    commit

---
+++ basics/01_basics.js
@@ -0,0 +1,23 @@
+// datatypes
+
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(NaN));
+
+//conversion
+
+console.log(Number("33"));
+console.log(Number("33abc"));
+console.log(Number(NaN));
+console.log(Number(null));
+console.log(Number(undefined));
+
+console.log(Boolean(34));
+
+//comparisions
+// there is inconsistency in the following comparisions so avoid them
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
